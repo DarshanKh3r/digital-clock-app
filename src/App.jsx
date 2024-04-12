63e792dba21927e7e06dc1aeda146985ae95973b
Darshan Kher <darshan.rzr@gmail.com>
2024-04-13 00:20:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import DigitalClock from './DigitalClock'
+
+function App() {
+ 
+
+  return (
+    <>
+     <DigitalClock />
+    </>
+  )
+}
+
+export default App
